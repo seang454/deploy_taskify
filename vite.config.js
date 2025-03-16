aa2686973b0d409acbee7ad97017be1a258c4732
ruppstudent <pengseangsim210@gmail.com>
2025-03-16 15:35:55 +0700

    chagne config in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   assetsInclude: ['**/*.JPG'],
+//   plugins: [
+//     react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   assetsInclude: ['**/*.JPG'],
-  plugins: [
-    react()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['yup']
+    }
+  }
 })
