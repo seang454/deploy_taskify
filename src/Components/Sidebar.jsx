123b7e30c810857f2d2779c0b0dcf96a51a5c5ae
ruppstudent <pengseangsim210@gmail.com>
2025-03-21 14:02:53 +0700

    New

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -70,12 +70,13 @@ export default function Sidebar() {
             <div>Notifications</div>
           </Link>
           <Link
-            className="flex justify-start py-4 pl-4 space-x-4 align-middle transition-all duration-500 hover:bg-primary hover:text-background"
+            className="flex justify-start py-4 pl-4 space-x-4 align-middle transition-all duration-500 hover:bg-yellow-200 hover:text-gray-900"
             to="/aboutus"
           >
-            <IoIosHelpCircleOutline className="w-5 h-5" />
+            <IoIosHelpCircleOutline className="w-5 h-5 transition-transform duration-500 hover:scale-110" />
             <div>About Taskify</div>
           </Link>
+
           {getAceAccessToken() && (
             <button
               className="flex justify-start py-4 pl-4 space-x-4 align-middle transition-all duration-500 hover:bg-primary hover:text-background"
@@ -87,85 +88,6 @@ export default function Sidebar() {
           )}
         </div>
       </nav>
-
-      {/* Mobile Menu Toggle Button */}
-      <div className={"absolute top-4 z-10"}>
-        <button
-          className="flex gap-10 p-5 mt-20 text-2xl rounded-full text-primary md:hidden"
-          onClick={toggleMenu}
-        >
-          {isOpen ? "" : <TiThMenu />} {/* Changes icon when open/closed */}
-        </button>
-      </div>
-
-      {/* Mobile Sidebar */}
-      {isOpen && (
-        <div className="absolute">
-          <nav className="fixed bottom-0 z-10 flex flex-col w-1/3 h-full py-2 mt-2 transition-transform bg-background text-txt16 place-content-between top-20 gap-y-10 md:hidden min-w-56">
-            {/* Close Button */}
-            <button
-              className="absolute top-0 right-0 flex gap-10 p-5 text-2xl rounded-full text-primary"
-              onClick={toggleMenu}
-            >
-              <AiOutlineClose />
-            </button>
-            {/* Navigation Links */}
-            <div className="flex flex-col mt-10">
-              <Link
-                to="/"
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-              >
-                <MdOutlineLeaderboard className="w-5 h-5" />
-                <div>Home Page</div>
-              </Link>
-              <Link
-                to="/dashboard"
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-              >
-                <MdOutlineLeaderboard className="w-5 h-5" />
-                <div>Workspace</div>
-              </Link>
-              <Link
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-                to="/member"
-              >
-                <IoPersonOutline className="w-5 h-5" />
-                <div>Members</div>
-              </Link>
-              <Link
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-                to="/userpf"
-              >
-                <IoSettingsOutline className="w-5 h-5" />
-                <div>User Profile</div>
-              </Link>
-              <Link
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-                to="/notification"
-              >
-                <IoMdNotificationsOutline className="w-5 h-5" />
-                <div>Notifications</div>
-              </Link>
-              <Link
-                className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-                to="/aboutus"
-              >
-                <IoIosHelpCircleOutline className="w-5 h-5" />
-                <div>About Taskify</div>
-              </Link>
-              {getAceAccessToken() && (
-                <button
-                  className="flex justify-start py-4 pl-4 space-x-4 transition-all duration-500 hover:bg-primary hover:text-background"
-                  onClick={handleLogout}
-                >
-                  <TiArrowLeftOutline className="w-5 h-5" />
-                  <div>Log Out</div>
-                </button>
-              )}
-            </div>
-          </nav>
-        </div>
-      )}
     </>
   );
 }
